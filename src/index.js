e7a239061206131fb4f4421dc02c3ddabce8f2a4
bborelli <borelli.bruna@gmail.com>
2024-11-06 15:00:22 +0000

    Injected more data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const conditionsMap = {
   "clear sky": "clear-sky",
   "few clouds": "few-clouds",
+  "scattered clouds": "few-clouds",
   "broken clouds": "broken-clouds",
+  "overcast clouds": "broken-clouds",
+  "shower rain": "rainy",
+  "light rain": "rainy",
   rain: "rainy",
+  "heavy rain": "rainy",
+  drizzle: "rainy",
+  thunderstorm: "rainy",
   snow: "snowy",
+  haze: "windy",
   mist: "windy",
+  fog: "windy",
+  smoke: "windy",
+  squall: "windy",
+  tornado: "windy",
+  cloudy: "broken-clouds",
 };
 
 const iconMap = {
   "clear-sky": "src/icons/sunny.png",
-  "few-clouds": "src/icons/cloudy.png",
+  "few-clouds": "src/icons/sunclouds.png",
   "broken-clouds": "src/icons/cloudy.png",
-  rainy: "src/icons/rain.png",
-  snowy: "src/icons/snow.png",
+  rainy: "src/icons/rainy.png",
+  snowy: "src/icons/snowy.png",
   windy: "src/icons/windy.png",
 };
 
@@ -47,3 +60,50 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = response.data.wind.speed;
   timeElement.innerHTML = formatDate(new Date(response.data.time * 1000));
 }
+
+function formatDate(date) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = days[date.getDay()];
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDescription(description) {
+  return description
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+function searchCity(city) {
+  const apiKey = "4fbe9b2c44d0f8a0833d1te403cbb78o";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios
+    .get(apiUrl)
+    .then(refreshWeather)
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      alert("Unable to retrieve weather information. Please try again.");
+    });
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  const searchInput = document.querySelector("#search-form-input");
+  if (searchInput.value) searchCity(searchInput.value);
+}
+
+document
+  .querySelector("#search-form")
+  .addEventListener("submit", handleSearchSubmit);
+searchCity("Lisbon");
