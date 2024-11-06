35abd12892fc5f9d13fdfa7e9176fefa3c348c1a
bborelli <borelli.bruna@gmail.com>
2024-11-06 14:57:19 +0000

    Added conditionsMap and iconMap for weather conditions

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const conditionsMap = {
+  "clear sky": "clear-sky",
+  "few clouds": "few-clouds",
+  "broken clouds": "broken-clouds",
+  rain: "rainy",
+  snow: "snowy",
+  mist: "windy",
+};
+
+const iconMap = {
+  "clear-sky": "src/icons/sunny.png",
+  "few-clouds": "src/icons/cloudy.png",
+  "broken-clouds": "src/icons/cloudy.png",
+  rainy: "src/icons/rain.png",
+  snowy: "src/icons/snow.png",
+  windy: "src/icons/windy.png",
+};
